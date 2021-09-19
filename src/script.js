cbb06c463d0517a00e13d59ddf5e272f94692af4
estravers25 <88214881+estravers25@users.noreply.github.com>
2021-09-18 19:44:57 -0600

    Removed celsius HTML & JS conversion code; updated time to  display as less than 12 (not military time)

--- src/script.js
+++ src/script.js
@@ -35,10 +35,14 @@ function displayCurrentTime() {
   if (hours < 10) {
     hours = `0${hours}`;
   }
+  if (hours > 12) {
+    hours = `${hours - 12}`;
+  }
   let minutes = now.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
+
   let currentTime = `${day} ${hours}:${minutes}`;
   let searchTime = document.querySelector("#current-time");
   searchTime.innerHTML = `${currentTime}`;
@@ -77,10 +81,10 @@ function displayCurrentData(response) {
   let weatherCityHeader = document.querySelector("#weather-city");
   weatherCityHeader.innerHTML = `${currentCity}`;
 
-  fahrenheitTemperature = response.data.main.temp;
+  /*fahrenheitTemperature = response.data.main.temp;
   fahrenheitLowTemp = response.data.main.temp_min;
   fahrenheitHighTemp = response.data.main.temp_max;
-  fahrenheitFeelsLikeTemp = response.data.main.feels_like;
+  fahrenheitFeelsLikeTemp = response.data.main.feels_like;*/
 
   let iconElement = document.querySelector(".current-emoji");
   iconElement.setAttribute(
@@ -114,64 +118,24 @@ function displayCurrentData(response) {
   getForecast(response.data.coord);
 }
 
-function convertToCelsius(event) {
-  event.preventDefault();
-
-  let temperatureElement = document.querySelector("#current-temp");
-  let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-
-  let fahrenheitLowElement = document.querySelector("#current-low");
-  let celsiusLowTemp = ((fahrenheitLowTemp - 32) * 5) / 9;
-  fahrenheitLowElement.innerHTML = Math.round(celsiusLowTemp);
-
-  let fahrenheitHighElement = document.querySelector("#current-high");
-  let celsiusHighTemp = ((fahrenheitHighTemp - 32) * 5) / 9;
-  fahrenheitHighElement.innerHTML = Math.round(celsiusHighTemp);
-
-  let fahrenheitFeelsLikeElement = document.querySelector("#feels-like");
-  let celsiusFeelsLikeTemp = ((fahrenheitFeelsLikeTemp - 32) * 5) / 9;
-  fahrenheitFeelsLikeElement.innerHTML = Math.round(celsiusFeelsLikeTemp);
-}
-
-function convertToFahrenheit(event) {
-  event.preventDefault();
-
-  let temperatureElement = document.querySelector("#current-temp");
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-
-  let lowTempElement = document.querySelector("#current-low");
-  lowTempElement.innerHTML = Math.round(fahrenheitLowTemp);
-
-  let highTempElement = document.querySelector("#current-high");
-  highTempElement.innerHTML = Math.round(fahrenheitHighTemp);
-
-  let feelsLikeElement = document.querySelector("#feels-like");
-  feelsLikeElement.innerHTML = Math.round(fahrenheitFeelsLikeTemp);
-}
-
-function getForecast(coordinates){
-  let apiKey="56a5727662e9674f972770adf6f30527";
+function getForecast(coordinates) {
+  let apiKey = "56a5727662e9674f972770adf6f30527";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
 
   axios.get(apiUrl).then(displayForecast);
 }
 
-function displayForecast(response){
+function displayForecast(response) {
   let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row justify-content-center" id="forecast">`;
-  forecast.forEach(function(forecastDay, index){
-    if (index < 5){
-      forecastHTML = forecastHTML + `<div class="col">        
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col">        
               <h4 class="weather-forecast-day">
                 ${formatForecastDay(forecastDay.dt)}
               </h4>
@@ -189,19 +153,21 @@ function displayForecast(response){
                   ${forecastDay.weather[0].main}
                 </div>
                 <div class="forecast-icon">
-                  <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="50"/>
+                  <img src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png" alt="" width="50"/>
                 </div>
               </p>
-            </div>`
+            </div>`;
     }
 
     forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML=forecastHTML;
-  })
+    forecastElement.innerHTML = forecastHTML;
+  });
 }
 
-function formatForecastDay(timestamp){
-  let date = new Date(timestamp*1000);
+function formatForecastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
@@ -213,17 +179,6 @@ form.addEventListener("submit", handleSubmit);
 let useCurrentLocation = document.querySelector("button");
 useCurrentLocation.addEventListener("click", getPosition);
 useCurrentLocation.addEventListener("click", displayCurrentTime);
-
-let fahrenheitTemperature = null;
-let fahrenheitLowTemp = null;
-let fahrenheitHighTemp = null;
-let fahrenheitFeelsLikeTemp = null;
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 displayCurrentTime();
 
